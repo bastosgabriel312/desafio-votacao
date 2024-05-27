9bc9d705fd05270c930a237e694daa92d04f7201
bastosgabriel312 <bastosgabriel312@gmail.com>
2024-05-27 14:57:08 -0300

    docs: criação da configuração responsável por habilitar o Swagger e o Log da aplicação

--- src/main/java/br/com/db/desafio_votacao/DesafioVotacaoApplication.java
+++ src/main/java/br/com/db/desafio_votacao/DesafioVotacaoApplication.java
@@ -2,8 +2,10 @@ package br.com.db.desafio_votacao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DesafioVotacaoApplication {
 
 	public static void main(String[] args) {
