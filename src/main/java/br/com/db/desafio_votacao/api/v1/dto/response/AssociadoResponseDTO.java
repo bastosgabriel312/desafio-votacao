1f8c129785f436311fa5aec55ae2926020517e4a
bastosgabriel312 <bastosgabriel312@gmail.com>
2024-05-27 23:04:51 -0300

    feat: criação dos controllers, dtos e dtos de resposta

---
+++ src/main/java/br/com/db/desafio_votacao/api/v1/dto/response/AssociadoResponseDTO.java
@@ -0,0 +1,17 @@
+package br.com.db.desafio_votacao.api.v1.dto.response;
+
+import br.com.db.desafio_votacao.model.Associado;
+import lombok.Data;
+
+@Data
+public class AssociadoResponseDTO {
+    private Long id;
+    private String nome;
+    private String cpf;
+
+    public AssociadoResponseDTO(Associado associado) {
+        this.id = associado.getId();
+        this.nome = associado.getNome();
+        this.cpf = associado.getCpf();
+    }
+}
